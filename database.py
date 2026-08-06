a2724e92b10f2dac95e1339109e6d841db6eccb6
Victor <vinegraodecamargo@gmail.com>
2026-08-05 21:11:15 -0300

    create sessionmaker

--- database.py
+++ database.py
@@ -1,6 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+
 
 db = create_engine("sqlite:///raw_data.db")
+
+SessionLocal = sessionmaker(
+    bind=db,
+    autoflush=False,
+    autocommit=False
+)
 
 Base = declarative_base()
