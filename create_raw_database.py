5c755e289827a011ba56fb72fcefcfaf0c9276dc
Victor <vinegraodecamargo@gmail.com>
2026-08-07 13:54:39 -0300

    add table Usuario and table RelatorioAbcVendas

--- create_raw_database.py
+++ create_raw_database.py
@@ -5,5 +5,7 @@ from models.consulta_estoque_eud import ConsultaEstoqueEUD
 from models.consulta_estoque_qdb import ConsultaEstoqueQDB
 from models.pedido_semanal_especial import PedidoSemanalEspecial
 from models.ruptura_total_bot import RupturaTotalBot
+from models.usuarios import Usuario
+from models.raw_relatorio_abc_vendas import RelatorioAbcVendas
 
 Base.metadata.create_all(bind=db)
