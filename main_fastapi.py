d5734b8548a4928bd7558fa6c7bad52a09c75122
Victor <vinegraodecamargo@gmail.com>
2026-08-08 01:00:34 -0300

    add auth router and CryptContext

--- main_fastapi.py
+++ main_fastapi.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from passlib.context import CryptContext
 
 app = FastAPI()
 
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 from report_routes import report_router
+from auth_routes import auth_router
 
 app.include_router(report_router)
+app.include_router(auth_router)
