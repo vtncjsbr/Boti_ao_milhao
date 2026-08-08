a43c64c23177e924c4f386ba25f50a099fc2284a
Victor <vinegraodecamargo@gmail.com>
2026-08-08 01:02:46 -0300

    organize code

--- create_user.py
+++ create_user.py
@@ -1,11 +1,10 @@
 from models.usuarios import Usuario
 from database import SessionLocal
 from passlib.context import CryptContext
-
-bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from main_fastapi import bcrypt_context
 
 senha_criptografada = bcrypt_context.hash("123")
-novo_usuario = Usuario(nome='bagas', senha=senha_criptografada, loja='CZ', cargo='Mega Brain')
+novo_usuario = Usuario(nome='vigas', senha=senha_criptografada, loja='CZ', cargo='cop tief')
 
 session = SessionLocal()
 session.add(novo_usuario)
