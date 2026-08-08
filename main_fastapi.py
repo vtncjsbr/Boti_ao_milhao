533619a0a193e819e67ba0b9edb5174e20066423
Victor <vinegraodecamargo@gmail.com>
2026-08-08 01:48:15 -0300

    load .env variables

--- main_fastapi.py
+++ main_fastapi.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 app = FastAPI()
 
