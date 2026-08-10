b107f1b5a3a7f796a37f4bdb5faa3382dc57b6d1
Victor <vinegraodecamargo@gmail.com>
2026-08-10 15:30:52 -0300

    add oauth2passwordbearer

--- main_fastapi.py
+++ main_fastapi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from dotenv import load_dotenv
 import os
@@ -11,6 +12,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 app = FastAPI()
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 from report_routes import report_router
 from auth_routes import auth_router
