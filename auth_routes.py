c218a9b24bc3077c3811df241e50924dc6d5d353
Victor <vinegraodecamargo@gmail.com>
2026-08-10 15:32:40 -0300

    create function refresh_token

--- auth_routes.py
+++ auth_routes.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, HTTPException, Depends
 from main_fastapi import bcrypt_context, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
 from models.usuarios import Usuario
-from database import pegar_sessao
+from dependencies import pegar_sessao, verificar_token
 from jose import jwt, JWTError
 from datetime import datetime, timedelta, timezone
 
 def criar_token(id_usuario, ducarao_token = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
     data_expiracao = datetime.now(timezone.utc) + ducarao_token
-    dic_info = {"sub": id_usuario, "exp": data_expiracao}
+    dic_info = {"sub": str(id_usuario), "exp": data_expiracao}
     jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
     return jwt_codificado
 
@@ -33,4 +33,12 @@ async def login(nome, senha, session = Depends(pegar_sessao)):
             "access_token": access_token,
             "refresh_token": refresh_token,
             "token_type": "Bearer"
+        }
+
+@auth_router.get("/refresh")
+async def refresh_token(usuario = Depends(verificar_token)):
+    access_token = criar_token(usuario.id)
+    return {
+        "access_token": access_token,
+        "token_type": "Bearer"
         }
