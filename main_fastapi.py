5130fa37c4772d8c79f7588861d4553df6023402
Victor <vinegraodecamargo@gmail.com>
2026-08-10 15:38:54 -0300

    change auth route form

--- main_fastapi.py
+++ main_fastapi.py
@@ -12,7 +12,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 app = FastAPI()
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/login")
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/login_form")
 
 from report_routes import report_router
 from auth_routes import auth_router
