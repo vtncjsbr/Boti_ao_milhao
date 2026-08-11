9b9096384a33d1a224b4c1e08fec08576a816c14
Victor <vinegraodecamargo@gmail.com>
2026-08-11 03:12:51 -0300

    remove useless endpoints and add relatorio_vendas endpoint

--- report_routes.py
+++ report_routes.py
@@ -1,18 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from dependencies import pegar_sessao, verificar_token
+from models.raw_relatorio_abc_vendas import RelatorioAbcVendas
+from database import db
+from sqlalchemy import select
+import pandas as pd
+from io import BytesIO
+from fastapi.responses import StreamingResponse
 
-report_router = APIRouter(prefix="/report", tags=["Report"])
+report_router = APIRouter(prefix="/report", tags=["Report"], dependencies=[Depends(verificar_token)])
 
-@report_router.get("/consulta_estoque")
-async def consultar_estoque():
+@report_router.get("/vendas")
+async def relatorio_vendas():
+    stmt = select(RelatorioAbcVendas)
 
-    return {"mensagem": "Relatório gerado"}
+    with db.connect() as conn:
+        df = pd.read_sql(stmt, con=conn)
 
-@report_router.get("/pedido_semanal")
-async def pedido_semanal():
+    arquivo = BytesIO()
 
-    return {"mensagem": "Relatório gerado"}
+    with pd.ExcelWriter(arquivo) as writer:
+        df.to_excel(writer, index=False, sheet_name="Vendas")
+    arquivo.seek(0)
 
-@report_router.get("/ruptura_total")
-async def ruptura_total():
-
-    return {"mensagem": "Relatório gerado"}
+    return StreamingResponse(arquivo, 
+                            media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
+                            headers={"Content-Disposition": "attachment; filename=relatorio_vendas.xlsx"
+            })
