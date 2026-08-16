71fa366dcf417a3133366dcffb5464f484e48e1b
Victor <vinegraodecamargo@gmail.com>
2026-08-16 19:38:38 -0300

    fix import models files

--- create_raw_database.py
+++ create_raw_database.py
@@ -1,10 +1,10 @@
 from database import Base, db
 
-from models.consulta_estoque_bot import ConsultaEstoqueBOT
-from models.consulta_estoque_eud import ConsultaEstoqueEUD
-from models.consulta_estoque_qdb import ConsultaEstoqueQDB
-from models.pedido_semanal_especial import PedidoSemanalEspecial
-from models.ruptura_total_bot import RupturaTotalBot
+from models.raw_consulta_estoque_bot import ConsultaEstoqueBOT
+from models.raw_consulta_estoque_eud import ConsultaEstoqueEUD
+from models.raw_consulta_estoque_qdb import ConsultaEstoqueQDB
+from models.raw_pedido_semanal_especial import PedidoSemanalEspecial
+from models.raw_ruptura_total_bot import RupturaTotalBot
 from models.usuarios import Usuario
 from models.raw_relatorio_abc_vendas import RelatorioAbcVendas
 
